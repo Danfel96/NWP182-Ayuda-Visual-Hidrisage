ae970e0ded006eb29d1e67ad5388ecf4e9cbd342
Danfel <dan.fel2496@gmail.com>
2023-09-11 02:52:32 -0500

    url

---
+++ Assets/Scritps/URLOpener.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class URLOpener : MonoBehaviour
+{
+    [SerializeField] string url;
+    public Button Btn { get; set; }
+
+    private void Start()
+    {
+        Btn = GetComponent<Button>();
+        if (Btn == null || string.IsNullOrEmpty(url)) return;
+        Btn.onClick.AddListener(OpenURL);
+    }
+    public void OpenURL() => Application.OpenURL(url);
+
+    public void OpenURL(string url) => Application.OpenURL(url);
+
+    //public void DialPhoneNumber(string phoneNumber) => OpenURL($"{Constants.Tel}{phoneNumber}");
+}

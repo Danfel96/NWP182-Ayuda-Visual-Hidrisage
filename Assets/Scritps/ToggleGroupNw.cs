9ac9b8ad0505abe1c17659ecedcc775d2ed05760
Danfel <dan.fel2496@gmail.com>
2023-09-13 15:27:43 -0500

    Content info

---
+++ Assets/Scritps/ToggleGroupNw.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Newronizer
+{
+    public class ToggleGroupNw : UnityEngine.UI.ToggleGroup
+    {
+        [SerializeField] UnityEvent onAnyToggleOn, onAnyToggleOff;
+        bool invokedToggleOn, invokedToggleOff;
+
+        private void Update()
+        {
+            if(AnyTogglesOn() && !invokedToggleOn)
+            {
+                invokedToggleOn = true;
+                invokedToggleOff = false;
+                print("On");
+                InvokeEvent(onAnyToggleOn);
+            }
+            else if(!AnyTogglesOn() && !invokedToggleOff)
+            {
+                invokedToggleOn = false;
+                invokedToggleOff = true;
+                print("Off");
+                InvokeEvent(onAnyToggleOff);
+            }
+
+        }
+
+        private void InvokeEvent(UnityEvent e)
+        {
+            if (e != null)
+                e.Invoke();
+        }
+    }
+}

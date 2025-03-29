9de1abcdadb3061d1b68bab1a9131d307a82de49
Sebastián Franco <regsebas@gmail.com>
2025-03-29 08:12:15 -0500

    [fix] video loader

---
+++ Assets/VideoLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class VideoLoader : MonoBehaviour
+{
+
+    [ContextMenu("GetPath")]
+    private void GetPathVideo() 
+    {
+        string videoPath = EditorUtility.OpenFilePanel("Selecciona un video", "", "mp4");
+    }
+}
